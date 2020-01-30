f5879b40e1f8d7f8fa6c41cd0e7f466081eaec81
Valentin <z-@xn--3ca.fr>
2020-01-30 14:01:48 +0100

    f

--- srcs/display/ft_display_c.c
+++ srcs/display/ft_display_c.c
@@ -7,12 +7,12 @@ void	ft_display_c(char c, t_flags *flags)
 
 	int tmp_w;
 
-	if (flags->width < 0)
+	tmp_w = flags->width;
+	if (tmp_w < 0)
 	{
 		flags->tiret = 1;
-		flags->width = -flags->width;
+		tmp_w = -tmp_w;
 	}
-	tmp_w = flags->width;
 	if ((flags->precision == 0) && flags->percent == 1)
 	{
 		if (flags->tiret == 0)
