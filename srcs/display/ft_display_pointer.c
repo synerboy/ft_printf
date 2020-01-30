d9cf7095c196d1c25ec286f7cc585acb54844083
Valentin <z-@xn--3ca.fr>
2020-01-30 14:42:42 +0100

    f

--- srcs/display/ft_display_pointer.c
+++ srcs/display/ft_display_pointer.c
@@ -42,9 +42,9 @@ void	ft_display_pointer(t_flags *flags, char *pt)
 		if (flags->zero && flags->width > 0 && flags->tiret == 0)
 			while (tmp_p-- > 0)
 				ft_putchar_fd('0', 2);
-		if (flags->zero && flags->width > 0 && flags->tiret == 0)
+		if (flags->zero && flags->width > 0 && flags->tiret == 0 && flags->point == 0)
 			while (tmp_w-- > 0)
-				ft_putchar_fd((flags->point == 0) ? '0' : ' ', 2);
+				ft_putchar_fd('0', 2);
 		ft_putpointer_fd(read, 2);
 		if (flags->tiret)
 			while (tmp_w-- > 0)
