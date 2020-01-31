9bfb07182db3476d8418f6840ccb9310266c4e91
Valentin BRAGEOT <vabrageo@e1r12p11.42.fr>
2020-01-31 19:12:29 +0100

    Norm error

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,17 @@
 /*   By: vabrageo <vabrageo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 12:11:26 by vabrageo          #+#    #+#             */
-/*   Updated: 2020/01/31 19:07:56 by vabrageo         ###   ########.fr       */
+/*   Updated: 2020/01/31 19:12:06 by vabrageo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char *st, ...)
+int			ft_printf(char *st, ...)
 {
-	va_list args;
+	va_list	args;
 	char	*tmp;
-	int 	i;
+	int		i;
 
 	i = 0;
 	tmp = st;
