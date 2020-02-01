01006397ac1e344661eb502bb53afd6b2babc53a
Synerboy <synerboy@MacBook-Pro-de-Synerboy.local>
2020-02-01 16:06:00 +0100

    f

--- srcs/bundle/ft_putpointer_maj.c
+++ srcs/bundle/ft_putpointer_maj.c
@@ -17,9 +17,7 @@ void	ft_putpointermaj_fd(__uint128_t n, int fd)
 	if (n <= 9)
 		ft_putchar_fd((n + '0'), fd);
 	else if (n >= 10 && n <= 16)
-	{
 		ft_putchar_fd(('A' - 10) + n, fd);
-	}
 	else if (n > 16)
 	{
 		ft_putpointermaj_fd(n / 16, fd);
